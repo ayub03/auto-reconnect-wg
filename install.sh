ef4da2dc0f439c03ad7cc9960ebcac89ac6d3165
ayub03 <65050338+ayub03@users.noreply.github.com>
2021-06-19 20:10:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 opkg update && opkg install fping
 wget --no-check-certificate "https://raw.githubusercontent.com/ayub03/auto-reconnect-wg/main/Internet%20hilink" -O /usr/bin/hilink.sh
 chmod a+x /usr/bin/hilink.sh
+sleep 2
+clear
+echo "sudah selesai"
+echo "untuk memulai silahkan jalankan perintah 'hilink.sh &'"
+echo "atau bisa dimasukan ke local startup 'sleep 20 && hilink.sh &"
+
